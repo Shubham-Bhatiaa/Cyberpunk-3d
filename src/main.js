268ce17239a974eef93357fb326d09ccacbe23d3
Shubham Bhatia <shubhambhatia534@gmail.com>
2025-05-15 02:55:17 +0530

    three.js components setup

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
 import './style.css'
 
+import * as THREE from 'three';
+
+//scene
+const scene = new THREE.Scene();
+
+//camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.z = 5;
+
+//objects
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: "red"});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#canvas"),
+    antialias: true,
+});
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // to get great performance on high pixel ratio displays without sacrificing resources
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//render
+renderer.render(scene, camera);
